2fc23aee4afd873be4c082451bcda720570b0cf8
LucasAAraujo <lucasalves3.0@hotmail.com>
2025-02-06 09:46:52 -0300

    Add Colaborador component and integrate with Time; update Formulario and ListaSuspensa for improved functionality

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,7 +5,8 @@ const ListaSuspensa = (props) => {
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
-            <select required={props.obrigatorio} value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                <option value=""></option>
                 <option value="">{props.placeholder}</option>
                 {props.itens.map(item => {
                     return <option key={item}>{item}</option>
